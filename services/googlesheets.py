fd42fa6e735f4cbc2f4dea2850b28ca951969efd
antONget <a.l.ponomare@mail.ru>
2025-04-02 22:14:45 +0300

    init

--- services/googlesheets.py
+++ services/googlesheets.py
@@ -2,15 +2,17 @@ import gspread
 import logging
 
 
-gp = gspread.service_account(filename='services/anketa.json')
+gp = gspread.service_account(filename='services/TAVI.json')
 # gp = gspread.service_account(filename='anketa.json')
 
 # Open Google spreadsheet
-gsheet = gp.open('@bazunovaleksey')
+# gsheet = gp.open('@bazunovaleksey')
+gsheet = gp.open('TAVI Регистр СЗФО РФ 2025')
 
 
 # select worksheet
-sheet = gsheet.worksheet("TAVI")
+# sheet = gsheet.worksheet("TAVI")
+sheet = gsheet.worksheet("2025")
 
 
 # добавить значения
